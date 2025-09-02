4383846d7ec426f1bf1ef66c21daf0a077bc6ef5
abhijit20-vd <abhijitdarade.vd@gmail.com>
2025-09-03 05:24:40 +0530

    menu impl

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num1, num2, res, choice;
+    cout<<"Calculator App : "<<endl;
+    cout<<"Enter two numbers : ";
+    cin>>num1>>num2;
+    cout<<"\nEnter choice : ";
+    cin>>choice;
+    switch(choice){
+        default : 
+            cout<<"Invalid choice"<<endl;
+        
+    }
+    return 0;
+}
